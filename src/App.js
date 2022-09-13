e2744913210c85c7c8291906f28c34f0706a8dd4
IdrisiAbdulla <abdullaidrisi00786@bbdu.ac.in>
2022-09-13 11:38:50 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from "./components/Register";
+import Login from "./components/Login";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import ProductCustomizer from "./components/ProductCustomizer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route element={<Register />} path="/Register" />
+        <Route element={<Login/>} path="/login" />
+        <Route element={<Home/>} path="/Home" />
+        <Route element={<ProductCustomizer/>} path="/ProductCustomizer" />
+        <Route element={<Home/>} path="*" />
+      </Routes>
+    
+
+      </BrowserRouter>
+      
   );
 }
 
